0aefb77d29aa2d7c0249c20518bccebec22380a2
coursecomputer <coursecomputer@outlook.com>
2019-10-28 19:09:59 +0100

    refactor: Rename some variable for more understanding

--- kmp/table.go
+++ kmp/table.go
@@ -5,26 +5,26 @@ type Table struct {
 }
 
 func (t *Table) Build(pattern []byte)  {
-	var i, j int
+	var idxPattern, lenMatch int
 	var lenPattern = len(pattern)
 
-	i = 1
+	idxPattern = 1
 	t.Content = make([]int, lenPattern + 1)
 	t.Content[0] = -1
-	for i < lenPattern {
-		if pattern[i] == pattern[j] {
-			t.Content[i] = t.Content[j]
+	for idxPattern < lenPattern {
+		if pattern[idxPattern] == pattern[lenMatch] {
+			t.Content[idxPattern] = t.Content[lenMatch]
 		} else {
-			t.Content[i] = j
-			j = t.Content[j]
+			t.Content[idxPattern] = lenMatch
+			lenMatch = t.Content[lenMatch]
 
-			for j >= 0 && pattern[i] != pattern[j] {
-				j = t.Content[j]
+			for lenMatch >= 0 && pattern[idxPattern] != pattern[lenMatch] {
+				lenMatch = t.Content[lenMatch]
 			}
 		}
-		i += 1
-		j += 1
+		idxPattern += 1
+		lenMatch += 1
 	}
 
-	t.Content[i] = j
+	t.Content[idxPattern] = lenMatch
 }
